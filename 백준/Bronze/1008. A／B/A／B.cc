fd75d0da169be9db26679a11d5d9cb4b3eb64df3
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 14:14:27 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1;
+	int num2;
+	double num3;
+
+	scanf("%d %d", &num1, &num2);
+
+	num3 = (double)num1 / (double)num2;
+
+	printf("%0.9lf\n", num3);
+
+	return 0;
+}
