de8ce3bfb7c6764faa3a9de40665635fa7f74061
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 14:11:59 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    long n;
+    long result;
+    
+    scanf("%ld", &n);
+    result = (n - 1) * n / 2;
+    
+    printf("%ld\n2", result);
+    
+    return 0;
+}
