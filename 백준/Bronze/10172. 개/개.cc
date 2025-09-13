604cd436e892d39150159e612aec161442906682
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 14:17:40 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char c1 = 'q';
+    char c2 = 'p';
+    int num1 = 0;
+    
+    printf("|\\_/|\n");
+    printf("|%c %c", c1, c2);
+    printf("|   /}\n");
+    printf("( %d )", num1);
+    printf("\"\"\"\\\n");
+    printf("|\"^\"`");
+    printf("    |\n");
+    printf("||_/=\\\\__|\n");
+    
+    return 0;
+}
