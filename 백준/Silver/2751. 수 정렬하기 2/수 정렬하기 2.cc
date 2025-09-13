cfca85e07ddb6adbf1b087ec968291e79a8a59f7
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 13:52:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 424 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int input[1000000];
+int compare(const void* a, const void* b)
+{
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 < num2)
+		return -1;
+	if (num1 > num2)
+		return 1;
+
+	return 0;
+}
+
+int main()
+{
+	int N;
+	
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &input[i]);
+
+	qsort(input, N, sizeof(int), compare);
+
+	for (int i = 0; i < N; i++)
+		printf("%d\n", input[i]);
+
+	return 0;
+}
