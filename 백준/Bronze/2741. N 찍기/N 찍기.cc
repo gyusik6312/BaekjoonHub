3783f4968c7493355e9d6545c5aa6517d6ef5bc6
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 14:15:46 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int count;
+    
+    scanf("%d", &count);
+    
+    for (int i = 1;i <= count; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
