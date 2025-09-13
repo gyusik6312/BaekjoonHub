614c1139a33754285de9c1c84390c4ead302b11d
gyusik6312 <111577017+gyusik6312@users.noreply.github.com>
2025-09-13 13:50:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    int num = -1;
+    int max = N / 5; // N보다 작은, 가장 큰 5의 배수
+
+    for (int i = max; i >= 0; i--) 
+    {
+        if ((N - i * 5) % 3 == 0) 
+        {
+            num = i + (N - i * 5) / 3;
+            break;
+        }
+        else 
+        {
+            if (i == 0)
+                break;
+        }
+    }
+
+    printf("%d", num);
+    return 0;
+}
